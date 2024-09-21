fa0f7058e0523a9eda5dca694d8e0cc2e84b2cfe
MohdSaquib114 <msmsaq114@gmail.com>
2024-09-21 09:49:32 +0530

    Change origin uri

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 4000
 
 app.use(express.json())
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://task-manager-8uxk.vercel.app/',
     credentials: true, } 
 ))
 app.use(cookieParser())
